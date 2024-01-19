410c5ac2406122d44941e008be635dbaf72facf3
Shaik Sameer <130442189+Shaik-Sameer77@users.noreply.github.com>
2024-01-19 09:00:36 +0530

    router practice

--- Beginner/reactRouter/src/components/Header.jsx/Header.jsx
+++ Beginner/reactRouter/src/components/Header.jsx/Header.jsx
@@ -115,25 +115,6 @@ const Header = () => {
                     Github
                   </NavLink>
                 </li>
-                {/* Include the Log in and Get started buttons in mobile menu */}
-                <li className="mt-4 lg:hidden">
-                  <Link
-                    to="#"
-                    className="block text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                    onClick={closeMenu}
-                  >
-                    Log in
-                  </Link>
-                </li>
-                <li className="lg:hidden">
-                  <Link
-                    to="#"
-                    className="block text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                    onClick={closeMenu}
-                  >
-                    Get started
-                  </Link>
-                </li>
               </ul>
             </div>
           </div>
@@ -238,7 +219,7 @@ const Header = () => {
                 <li>
                   <Link
                     to="#"
-                    className="block text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                    className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                     onClick={closeMenu}
                   >
                     Get started
