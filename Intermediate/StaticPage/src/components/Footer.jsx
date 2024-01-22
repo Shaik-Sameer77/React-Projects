0d5991ab579ca8d56b821b2743466619920f7e05
Shaik Sameer <130442189+Shaik-Sameer77@users.noreply.github.com>
2024-01-22 22:43:39 +0530

    done

---
+++ Intermediate/StaticPage/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/sameer-shaik-7a96881b7/" target={"blank"}>
+            LinkedIn
+          </a>
+          <a href="https://github.com/Shaik-Sameer77" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
